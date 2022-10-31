e22aaf0d9cdb36f803ccbf79506736c86cf9f8e1
davidmccormick27410 <davidmccormick27410@gmail.com>
2022-10-31 08:43:18 -0400

    Moved password to external file

---
+++ lodestar-ls_meter.ps1
@@ -0,0 +1,54 @@
+#Parameters
+$OracleDLLPath = "F:\downloads\Oracle.ManagedDataAccess.dll"
+
+#The oracle DataSource as you would compile it in TNSNAMES.ORA
+$datasource = "(DESCRIPTION =
+               (ADDRESS_LIST =
+               (ADDRESS = 
+               (PROTOCOL = TCP)
+               (HOST = hostingdb0p.its.unc.edu)(PORT = 1521)))
+               (CONNECT_DATA =
+               (SERVICE_NAME = ldstrprd.unc.edu)))"
+
+$username = "pwrline"
+$password = Get-Content C:\UNC\lodestar.txt
+
+$queryStatment = "SELECT uidmeter, meterid FROM meter" #Be careful not to terminate it with a semicolon, it doesn't like it
+
+$destinationInstance = "localhost"
+$destinationDB = "BFD"
+$destinationSchema ="Staging"
+$destinationTable = "LS_METER"
+
+#Actual Code
+
+#Load Required Types and modules
+Add-Type -Path $OracleDLLPath
+Import-Module SqlServer
+
+#Create the connection string
+$connectionstring = 'User Id=' + $username + ';Password=' + $password + ';Data Source=' + $datasource 
+
+#Create the connection object
+$con = New-Object Oracle.ManagedDataAccess.Client.OracleConnection($connectionstring)
+
+#Create a command and configure it
+$cmd = $con.CreateCommand()
+$cmd.CommandText = $queryStatment
+$cmd.CommandTimeout = 3600 #Seconds
+$cmd.FetchSize = 10000000 #10MB
+
+#Creates a data adapter for the command
+$da = New-Object Oracle.ManagedDataAccess.Client.OracleDataAdapter($cmd);
+#The Data adapter will fill this DataTable
+$resultSet = New-Object System.Data.DataTable
+
+#Only here the query is sent and executed in Oracle 
+[void]$da.fill($resultSet)
+
+#Close the connection
+$con.Close()
+
+
+#Now you can do anything with the data inside of $resultset, we're uploading it to SQL Server just because
+$resultSet | Write-DbaDbTableData -SqlInstance $destinationInstance -Database $destinationDB -Schema $destinationSchema -Table $destinationTable -Truncate  -BatchSize 10000 -EnableException
